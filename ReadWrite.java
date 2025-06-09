f60806aab93764e696b272573313a1a95c4625e0
Daniel Bryant <1996.daniel.bryant@gmail.com>
2025-06-10 01:07:27 +0200

    Skrev om klassen. Allt är uppdelat i metoder

--- ReadWrite.java
+++ ReadWrite.java
@@ -6,24 +6,49 @@ import java.util.Scanner;
 
 public class ReadWrite {
 
-
-    public static void main(String[] args) {
-        Counter count = new Counter();
-        Scanner sc = new Scanner(System.in);
-        String entry = "";
+    private Scanner scanner = new Scanner(System.in);
 
 
-        while (!Objects.equals(entry, "stop")) {
-            entry = sc.nextLine();
-            count.addRow();
-            count.addChars(entry);
-
-        }
-        count.charSum();
-        System.out.println("You have typed a total of " + count.getCharacters() + " characters over " + count.getRows() + " row(s)");
-
-
+    public String read() {
+        return scanner.nextLine();
     }
 
+    public void writeCharacters(Counter counter) {
+        System.out.println("you typed a total of " + counter.getCharacters() + " characters");
+    }
+
+    public void writeRows(Counter counter) {
+        System.out.println("you have typed messages over " + counter.getRows() + " rows");
+    }
+
+    public void writeWords(Counter counter) {
+        System.out.println("a total of " + counter.getWordsAmount() + " words have been typed");
+    }
+
+    public void writeLongestWord(Counter counter) {
+        System.out.println("the longest word was \"" + counter.getLongestWord() + "\", congratulations!");
+    }
+}
+
+/*
+    Counter count = new Counter();
+
+    String entry = "";
+
+
+        while(!Objects.equals(entry,"stop"))
+
+    {
+        entry = sc.nextLine();
+        count.addRow();
+        count.addChars(entry);
+
+    }
+        count.charSum();
+        System.out.println("You have typed a total of "+count.getCharacters()+" characters over "+count.getRows()+" row(s)");
 
 }
+
+
+
+*/
